7f92975f59f8bfde48183de31baa87d64546a1bc
Horia Sandu <horiasandu1@gmail.com>
2023-09-30 17:56:15 +0000

    initial commit for signin page

---
+++ ekoinsight-frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SignIn from "./SignIn";
+
+function App() {
+  return (
+    <div>
+      <SignIn></SignIn>
+      <h1>Hesdsy</h1>
+    </div>
+  );
+}
+
+export default App;
