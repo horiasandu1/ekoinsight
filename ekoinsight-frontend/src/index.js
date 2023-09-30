3a697fe0b4c0781d0ea23c302cfd1dae01de56ba
Horia Sandu <horiasandu1@gmail.com>
2023-09-30 18:56:56 +0000

    Working react router leading to signin page

--- ekoinsight-frontend/src/index.js
+++ ekoinsight-frontend/src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import SignIn from './SignIn';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />}></Route>
+          <Route path="signin" element={<SignIn/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <SignIn />
+  <>
+    <App />
+  </>
 );
-
