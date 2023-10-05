4580322bcd8674fd6fe29bc1d9b029c5f8a6e506
Horia Sandu <horiasandu1@gmail.com>
2023-10-05 11:35:14 +0000

    progress on home page and reworked navbar

--- ekoinsight_frontend/src/pages/Home/index.js
+++ ekoinsight_frontend/src/pages/Home/index.js
@@ -41,9 +41,9 @@ function Home() {
       <DefaultNavbar
         routes={routes}
         action={{
-          type: "external",
-          route: "https://www.creative-tim.com/product/material-kit-react",
-          label: "free download",
+          type: "internal",
+          route: "/pages/authentication/sign-in",
+          label: "Sign In",
           color: "info",
         }}
         sticky
